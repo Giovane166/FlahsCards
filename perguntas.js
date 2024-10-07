7a462ba1e63c61bab317bc236ab006b9e65fadbf
Giovane <103274655+Giovane166@users.noreply.github.com>
2024-10-07 07:41:08 -0300

    gjmgndcn

---
+++ perguntas.js
@@ -0,0 +1,108 @@
+criaCartao(
+    'Química',
+    'O que são óxidos?',
+    'Compostos binários, sendo iônicos ou moleculares.')
+
+criaCartao(
+    'Química',
+    'Os óxidos ácidos em meio aquoso produzem ________ (ácidos/bases).',
+    'Ácidos')    
+
+    
+criaCartao(
+    'Química',
+    'Um óxido anfótero somado a uma base ou ácido forte dá origem a sal e água.',
+    'Verdadeiro.')    
+
+    
+criaCartao(
+    'Física',
+    'O qu se estuda em Termodinâmica?',
+    'Relação entre calor e o trabalho ocorrido em dados fenômenos.')    
+
+    
+criaCartao(
+    'Física',
+    'Caso um gás seja confinado e sofra expansão, o trabalho realizado é ________ (positivo/negativo).',
+    'Positivo.')    
+
+    
+criaCartao(
+    'Física',
+    'Caso a variação de temperatura interna (∆U) seja positiva, o gás esquentará.',
+    'Verdadeiro.')    
+
+    
+criaCartao(
+    'Língua Portuguesa',
+    'Quais são os 2 tipos de concordância?',
+    'Verbal e Nominal.')    
+
+    
+criaCartao(
+    'Língua Portuguesa',
+    'Quando ocorre concordância nominal?',
+    'Quando ocorre a flexão de um termo para concordar com um substantivo.')    
+
+    
+criaCartao(
+    'Língua Portuguesa',
+    'Quando ocorre concordância verbal?',
+    'Quando se dá a flexão de um verbo para concordar com o sujeito.')    
+
+criaCartao(
+    'Biologia',
+    'O Fixismo também é conhecido como?',
+    'Criacionismo')    
+
+criaCartao(
+    'Biologia',
+    'O que o FIxismo defende?',
+    'O Fixismo defende que os seres vivos são criaturas imutáveis, permanentes e sem relação de origem comum.')    
+
+criaCartao(
+    'Biologia',
+    'O que o Evolucionismo defende?',
+    'Que os seres vivos existentes atualmente são frutos de modificações de seres previamente existentes.')    
+
+criaCartao(
+    'Geografia',
+    'O capitalismo comercial, arrecadava capital através da…',
+    'ampliação do comércio e da exploração do ouro e da prata.')
+
+criaCartao(
+    'Geografia',
+    'As nações ________ (européias/asiáticas) lideraram o processo de expansão.',
+    'Européias.')    
+
+criaCartao(
+    'Geografia',
+    'Qual tipo surgiu após o capitalismo comercial?',
+    'Industrial.')    
+
+criaCartao(
+    'Sociologia',
+    'Para os antropólogos a cultura sempre se declina para o _______ (singular/plural).',
+    'Plural.')    
+
+criaCartao(
+    'Sociologia',
+    'Como pode ser produzida a cultura?',
+    'Por meio da interação social entre indivíduos.')
+
+criaCartao(
+    'Sociologia',
+    'Pelo que é representada a Cultura Material?',
+    'Pelas construções humanas.')
+
+criaCartao(
+    'História',
+    'Qual grupo social surgiu a partir do reaparecimento do comércio?',
+    'A Burguesia'
+)    
+
+criaCartao(
+    'História',
+    'O que foram as Cruzadas?',
+    'Expedições de caráter religioso e militar com o objetivo de expulsar muçulmanos da terra santa.'
+)
